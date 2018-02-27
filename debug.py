fd7d01b084a2617c57a44e4a39d9b1cd697e888c
Yohei Koga <monotaro_EX@hotmail.com>
2018-02-28 00:23:28 +0900

    test code

--- debug.py
+++ debug.py
@@ -12,14 +12,17 @@ def compareCandP(ch0,ch1, batch, dshape):
     chainer_dconv = Deconvolution2DLayer(ch0,ch1,ksize,stride,pad)
     pytorch_dconv = nn.ConvTranspose2d(ch0,ch1,ksize,stride,pad,output_padding=1)
 
-    w = np.random.rand(ch0,ch1,ksize,ksize)
-    b = np.random.rand(ch1)
+    # w = np.random.rand(ch0,ch1,ksize,ksize)
+    # b = np.random.rand(ch1)
+    w = np.random.randint(0,255,(ch0,ch1,ksize,ksize))
+    b = np.random.randint(0, 255, ch1)
     chainer_dconv.c.W.data = w
     chainer_dconv.c.b.data = b
     pytorch_dconv.weight.data = torch.FloatTensor(w)
     pytorch_dconv.bias.data = torch.FloatTensor(b)
 
-    data = np.random.rand(batch, ch0, dshape[0],dshape[1])
+    # data = np.random.rand(batch, ch0, dshape[0],dshape[1])
+    b = np.random.randint(0,255, (batch, ch0, dshape[0],dshape[1]))
 
     c_result = chainer_dconv(Variable(data)).data
     p_result = pytorch_dconv(Variable_pt(torch.FloatTensor(data))).data.numpy()
